2723ab56998e81803b3d733a99baae75966e9ead
Eden <eden.s.mesfun@gmail.com>
2019-03-08 22:24:25 -0500

    Updates: >>Changed npm permissions, deleted node_modules and package-lock.json and ran npm install, >>Imported MapImageLayer and added it and sublayers to existing map of USA

--- src/js/components/MapView.js
+++ src/js/components/MapView.js
@@ -1,13 +1,16 @@
 import { viewCreated, getItemInfo } from 'js/actions/mapActions';
 import { MAP_OPTIONS, VIEW_OPTIONS } from 'js/config';
+
 import LocateModal from 'js/components/modals/Locate';
 import ShareModal from 'js/components/modals/Share';
 import Spinner from 'js/components/shared/Spinner';
 import Controls from 'js/components/Controls';
+
 import MapView from 'esri/views/MapView';
 import React, { Component } from 'react';
 import appStore from 'js/appStore';
 import EsriMap from 'esri/Map';
+import MapImageLayer from "esri/layers/MapImageLayer";
 
 export default class Map extends Component {
   displayName: 'Map';
@@ -19,6 +22,55 @@ export default class Map extends Component {
     this.unsubscribe = appStore.subscribe(this.storeDidUpdate);
 
     const map = new EsriMap(MAP_OPTIONS);
+
+    var layer = new MapImageLayer({
+      url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer",
+      sublayers: [
+        {
+          // counties
+          id: 3,
+          visible: false
+        },
+        { // state borders
+          id: 2,
+          visible: false
+        },
+        { 
+          // highways
+          id: 1,
+          visible: true,
+          popupEnabled: true
+        },
+        {
+          //  cities
+          id: 0,
+          visible: true,
+          popupEnabled: true
+        }
+      ]
+    });
+    
+    // var layer = new MapImageLayer({
+    //   url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer",
+    //   sublayers: [  // autocasts as a Collection of Sublayers
+    //     { // sets a definition expression on sublayer 0
+    //       id: 0,
+    //       definitionExpression: "pop2000 > 40000000"
+    //     },
+    //     {  // creates a dynamic data layer
+    //       source: {
+    //         type: dynamic-layer,
+    //         dataSource: {
+    //           type: "table",
+    //           workspaceId: "MyDatabaseWorkspaceIDSSR2",
+    //           dataSourceName: "ss6.gdb.Railroads"
+    //         }
+    //       }
+    //     }
+    //   ]
+    // });
+
+    map.add(layer);
 
     // Create our map view
     const promise = new MapView({
